411ac98e5122455863c9d17c61bcc6bc439d1dcc
narias1999 <nicolasarias870@gmail.com>
2020-07-05 18:03:36 -0500

    Responsive

--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { DarkModeButton } from './../DarkModeButton';
 import { LanguageButton } from './../LanguageButton';
 import { NavLink } from './../NavLink';
@@ -6,22 +8,30 @@ import logo from './../../assets/images/logo.svg';
 import './../../styles/components/sections/Navbar.scss';
 
 export function Navbar({ links }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+  const closeNav = () => setIsOpen(false);
+
   return (
-    <nav className='Navbar'>
-      <div className='Navbar-logo'>
+    <nav className={`Navbar ${isOpen ? 'open' : ''}`}>
+      <button className='Navbar-burger' onClick={toggleOpen}>
+        <FontAwesomeIcon size='lg' icon={isOpen ? faTimes : faBars} />
+      </button>
+      <div className='Navbar-logo' onClick={closeNav}>
         <NavLink to='#home'>
           <img src={logo} alt='Nicolás Arias logo' />
         </NavLink>
       </div>
       <div className='Navbar-right'>
         <ul className='Navbar-items'>
-          <li className='Navbar-item'>
+          <li className='Navbar-item' onClick={closeNav}>
             <NavLink to='#achievements'>{links.achievements}</NavLink>
           </li>
-          <li className='Navbar-item'>
+          <li className='Navbar-item' onClick={closeNav}>
             <NavLink to='#background'>{links.background}</NavLink>
           </li>
-          <li className='Navbar-item'>
+          <li className='Navbar-item' onClick={closeNav}>
             <NavLink to='#skills'>{links.skills}</NavLink>
           </li>
         </ul>
